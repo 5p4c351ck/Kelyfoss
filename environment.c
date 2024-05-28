0065d311e4f641a48a7d638ecfc32d2c0cd35789
5p4c351ck <kntazanis@gmai.com>
2024-05-29 02:15:28 +0300

    set environment variables

--- environment.c
+++ environment.c
@@ -1,24 +1,27 @@
 #include "stdlib.h"
 #include <stdio.h>
+#include <string.h>
 
 #include "environment.h"
 
-static int set_env_var(const char* str, env_vars *variables);
-static int resize_var_buffer(char **buffer);
+static int set_env_var(char* str, env_vars *variables);
+static int resize_var_buffer(env_vars *variables);
 
 
 environment* initialize_environment(void){
         environment *env = malloc(sizeof(*env));
         if(!env){
-                perror("environment.h line 11 malloc error");
+                perror("Environment memory allocation error");
                 exit(EXIT_FAILURE);
         }
 
         env->variables.buffer = malloc(INITIAL_CAPACITY * sizeof(char*));
         if(!env->variables.buffer){
-                perror("environment.h line 17 malloc error");
+                perror("Environmental variable buffer memory allocation error");
                 exit(EXIT_FAILURE);
         }
+        env->variables.size = 0;
+        env->variables.capacity = INITIAL_CAPACITY;
         return env;
 }
 
@@ -31,4 +34,47 @@ void destroy_environment(environment* env){
                 free(env->variables.buffer);
                 free(env);
         }
+}
+
+
+static int set_env_var(char* str, env_vars *variables){
+        if(variables){
+                for(unsigned int i = 0; i < variables->size; i++){
+                        char *var = variables->buffer[i];
+                        char *eq_sign = strchr(var, '=');
+                        if(eq_sign){
+                                size_t len = eq_sign - var;
+                                if(len == strlen(str) && (!strncmp(var, str, len))){
+                                                variables->buffer[i] = str;
+                                                return 0;
+                                }
+                        }         
+                }
+                if(variables->size == variables->capacity){
+                        if(resize_var_buffer(variables) == -1){
+                                return -1;
+                        }
+                        variables->buffer[variables->size++] = str;
+                        return 0;
+                }
+                else{
+                        variables->buffer[variables->size++] = str;
+                        return 0;  
+                }
+        }
+        return -1;
+}
+
+static int resize_var_buffer(env_vars *variables){
+        if(variables){
+                variables->capacity *= 2;
+                char ** temp = realloc(variables->buffer, variables->capacity * sizeof(char*));
+                if(temp){
+                        variables->buffer = temp;
+                        return 0;
+                }
+                variables->capacity /= 2;
+                return -1;
+        }
+        return -1;
 }
